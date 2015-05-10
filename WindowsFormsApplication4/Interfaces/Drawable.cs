6548bf911416c6e17913d0ce2c37beeafd0ff27f
makedon4e <martinboncanoski@gmail.com>
2015-05-10 22:35:55 +0200

    new changes

---
+++ WindowsFormsApplication4/Interfaces/Drawable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace HotDogBush
+{
+    public interface Drawable
+    {
+        void Draw(Graphics g);
+    }
+}
